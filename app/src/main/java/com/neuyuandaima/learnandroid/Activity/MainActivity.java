b4979866506f6c0e7eee80eac86d82e627302417
yule <neuyuandiama@outlook.com>
2015-08-20 17:24:04 +0800

    2015/08/20

--- app/src/main/java/com/neuyuandaima/learnandroid/Activity/MainActivity.java
+++ app/src/main/java/com/neuyuandaima/learnandroid/Activity/MainActivity.java
@@ -1,48 +1,73 @@
 package com.neuyuandaima.learnandroid.Activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.TabHost;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.neuyuandaima.learnandroid.AppManager;
 import com.neuyuandaima.learnandroid.R;
 import com.neuyuandaima.learnandroid.ui.NavigationDrawerFragment;
 
-import butterknife.Bind;
-import butterknife.ButterKnife;
-
-public class MainActivity extends Activity implements TabHost.OnTabChangeListener,View.OnClickListener,View.OnTouchListener{
-	@Bind(R.id.drawer_layout)DrawerLayout drawerLayout;
-	@Bind(R.id.navigation_drawer)NavigationDrawerFragment mNavigationDrawerFragment;
-	private CharSequence mTitle;
+public class MainActivity extends AppCompatActivity implements NavigationDrawerFragment.NavigationDrawerCallbacks{
+	private NavigationDrawerFragment mNavigationDrawerFragment;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		ButterKnife.bind(this);
 		initView();
 		AppManager.getInstance().addActivity(this);
+		ActionBar ab = getSupportActionBar();
+		ab.setDisplayHomeAsUpEnabled(true);
+		ab.setHomeButtonEnabled(true);
 	}
+
 
 	private void initView() {
-		mTitle=getTitle();
+		mNavigationDrawerFragment=(NavigationDrawerFragment)getFragmentManager().findFragmentById(R.id.navigation_drawer);
+		mNavigationDrawerFragment.setUp(R.id.navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout));
 	}
 
 	@Override
-	public void onClick(View v) {
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.main_activity_menu, menu);
 
+		if (!mNavigationDrawerFragment.isDrawerOpen()){
+			restoreActionBar();
+			return true;
+		}
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	public void restoreActionBar(){
+		ActionBar bar=getSupportActionBar();
+		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+		bar.setDisplayShowTitleEnabled(true);
 	}
 
 	@Override
-	public void onTabChanged(String tabId) {
-
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()){
+			case R.id.search:
+				Toast.makeText(this,"search",Toast.LENGTH_SHORT).show();
+				break;
+			case android.R.id.home:
+				if (mNavigationDrawerFragment.isDrawerOpen()){
+					mNavigationDrawerFragment.closeDrawerMenu();
+				}else {
+					mNavigationDrawerFragment.openDrawerMenu();
+				}
+			default:
+				break;
+		}
+		return super.onOptionsItemSelected(item);
 	}
 
 	@Override
-	public boolean onTouch(View v, MotionEvent event) {
-		return false;
+	public void onNavigationDrawerItemSelected(int position) {
+
 	}
 }
--- app/src/main/java/com/neuyuandaima/learnandroid/ui/NavigationDrawerFragment.java
+++ app/src/main/java/com/neuyuandaima/learnandroid/ui/NavigationDrawerFragment.java
@@ -1,20 +1,141 @@
 package com.neuyuandaima.learnandroid.ui;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
+import com.neuyuandaima.learnandroid.R;
 import com.neuyuandaima.learnandroid.base.BaseFragment;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 /**
  * Created by neuyuandaima on 2015/8/19.
  */
 public class NavigationDrawerFragment extends BaseFragment implements View.OnClickListener{
+	private View mDrawerListView;
+	private DrawerLayout mDrawerLayout;
+	private View mFragmentContainerView;
+	private ActionBarDrawerToggle mDrawerToggle;
+	private int mCurrentSelectedPosition = 0;
+	private NavigationDrawerCallbacks mCallbacks;
+	@Bind(R.id.menu_item_blog)View mMenu_item_bolg;
+	@Bind(R.id.menu_item_git)View mMenu_item_git;
+	@Bind(R.id.menu_item_opensoft)View mMenu_item_opensoft;
+	@Bind(R.id.menu_item_quests)View mMenu_item_quests;
 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		selectItem(mCurrentSelectedPosition);
+	}
 
+	private void selectItem(int position) {
+		mCurrentSelectedPosition = position;
+		if (mDrawerLayout!=null){
+			mDrawerLayout.closeDrawer(mFragmentContainerView);
+		}
+		if (mCallbacks!=null){
+			mCallbacks.onNavigationDrawerItemSelected(position);
+		}
+	}
 
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		mDrawerListView=inflater.inflate(R.layout.fragment_navigation_drawer,container,false);
+		mDrawerListView.setOnClickListener(this);
+		ButterKnife.bind(this, mDrawerListView);
+		initView(mDrawerListView);
+		return mDrawerListView;
+	}
+	public void openDrawerMenu() {
+		mDrawerLayout.openDrawer(mFragmentContainerView);
+	}
+	public void closeDrawerMenu(){mDrawerLayout.closeDrawer(mFragmentContainerView);}
 
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()){
+			case R.id.menu_item_quests:
 
+		}
+		mDrawerLayout.postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				mDrawerLayout.closeDrawers();
+			}
+		}, 800);
+	}
 
+	@Override
+	public void initView(View view) {
+		super.initView(view);
+		mMenu_item_bolg.setOnClickListener(this);
+		mMenu_item_git.setOnClickListener(this);
+		mMenu_item_opensoft.setOnClickListener(this);
+		mMenu_item_quests.setOnClickListener(this);
+	}
 
+	public void setUp(int fragmentId,DrawerLayout drawerLayout){
+		mFragmentContainerView=getActivity().findViewById(fragmentId);
+		mDrawerLayout=drawerLayout;
+
+		mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+		ActionBar actionBar=getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setHomeButtonEnabled(true);
+
+		mDrawerToggle=new ActionBarDrawerToggle(getActivity(),mDrawerLayout,null
+						,R.string.drawer_open,R.string.drawer_close){
+			@Override
+			public void onDrawerOpened(View drawerView) {
+				super.onDrawerOpened(drawerView);
+				getActivity().invalidateOptionsMenu();
+			}
+
+			@Override
+			public void onDrawerClosed(View drawerView) {
+				super.onDrawerClosed(drawerView);
+				getActivity().invalidateOptionsMenu();
+			}
+		};
+
+		mDrawerLayout.post(new Runnable() {
+			@Override
+			public void run() {
+				mDrawerToggle.syncState();
+			}
+		});
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+	}
+	public boolean isDrawerOpen() {
+		return mDrawerLayout != null
+				&& mDrawerLayout.isDrawerOpen(mFragmentContainerView);
+	}
+
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
+		try {
+			mCallbacks = (NavigationDrawerCallbacks) activity;
+		} catch (ClassCastException e) {
+			throw new ClassCastException(
+					"Activity must implement NavigationDrawerCallbacks.");
+		}
+	}
+
+	private ActionBar getActionBar(){
+		return ((AppCompatActivity) getActivity()).getSupportActionBar();
+	}
 	public interface NavigationDrawerCallbacks{
 		void onNavigationDrawerItemSelected(int position);
 	}
